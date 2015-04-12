6f299000e4a6ee6e2e5c166d48dfa2ce2c21736d
dbarelop <dbarelop@gmail.com>
2015-04-12 05:55:22 +0200

    Added support for notifications

--- server.js
+++ server.js
@@ -27,19 +27,22 @@ require('./lib/config/express')(app);
 // Routing
 require('./lib/routes')(app);
 
-
 //WebSocket   Starts the 
 var websocketServer = require('./lib/socketio')(app);
 websocketServer.listen(config.port);
 
+var multer = require('multer');
+app.use(multer({ dest: './uploads/' }));
+app.post('/upload-image', function(req, res) {
+    console.log('subido');
+    res.end('ok');
+});
 
 console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
 
 //error logging with raven
 var raven = require('raven');
 app.use(raven.middleware.express('https://db4f99d9238948be9d1e2fa85b34084a:6d031ec71a6a41c3bdea826ac9e55db9@app.getsentry.com/22998'));
-
-
 
 // Expose app
 var exports = module.exports = app;
